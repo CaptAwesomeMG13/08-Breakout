7522168180f1c0079e3429ab212e113e78401d0f
Mitch <mggilchrist@att.net>
2019-11-12 21:26:25 -0500

    Final Fail

--- Scripts/paddle.gd
+++ Scripts/paddle.gd
@@ -1,19 +1,50 @@
 extends KinematicBody2D
 
+onready var _shape = $ColorRect.get_rect().size
+onready var _view = get_viewport().get_visible_rect().size
+onready var _collision_transform = $CollisionShape2D.get_transform().get_scale()
+
+export var distort_x = 2.0
+export var distort_y = 1.2
+
+var _target = position
 var new_ball = preload("res://Scenes/Ball.tscn")
 
 func _ready():
- set_process_input(true)
+	set_process_input(true)
+	position.y = -30
+	$Tween.interpolate_property(self, "position", position, _target, 1.0, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+	$Tween.start()
 
 func _physics_process(delta):
- var mouse_x = get_viewport().get_mouse_position().x
- position = Vector2(mouse_x, position.y)
+	var mouse_x = get_viewport().get_mouse_position().x
+	var shape = $ColorRect.get_rect().size
+	var view = get_viewport().get_visible_rect().size
+	var target = get_viewport().get_mouse_position().x
+	if target > view.x - shape.x / 2:
+		target = shape.x / 2
+	if target > view.x - shape.x / 2:
+		target = view.x - shape.x / 2
+	
+	if target != position.x:
+		var x = position.x +((target - position.x)*.2)
+		var w = 1 + (distort_x * (abs(target - position.x) / _view.x))
+		var h = 1 - (1/distort_y * (abs(target - position.x) / _view.y))
+		_change_size(w,h)
+		position = Vector2(x, position.y)
+	else:
+		_change_size(1,1)
+		
+func _change_size(w,h):
+	$colorRect.rect_scale = Vector2(w,h)
+	$CollisionShape2D.set_scale(Vector2(_collision_transform.x*w,  _collision_transform.y*h))
+	
 
 func _input(event):
- if event is InputEventMouseButton and event.pressed:
-  if not get_parent().has_node("Ball"):
-   var ball = new_ball.instance()
-   ball.position = position - Vector2(0, 32)
-   ball.name = "Ball"
-   ball.linear_velocity = Vector2(200, -200)
-   get_parent().add_child(ball)
+	if event is InputEventMouseButton and event.pressed:
+		if not get_parent().has_node("Ball"):
+			var ball = new_ball.instance()
+			ball.position = position - Vector2(0, 32)
+			ball.name = "Ball"
+			ball.linear_velocity = Vector2(200, -200)
+			get_parent().add_child(ball)
--- Scripts/tile.gd
+++ Scripts/tile.gd
@@ -10,6 +10,16 @@ var yellow = preload("res://Assets/tile_yellow.png")
 onready var sprite = get_node("Sprite")
 var score = 10
 
+var _dying = 0.0
+var _dying_delta = 0.1
+var _dying_color_delta = 0.05
+var _dying_rotate = 0.0
+var _dying_rotate_delta = rand_range(-.03,.03)
+var _dying_threshhold = 10
+
+onready var t = $Timer
+onready var _target = position
+
 func _ready():
 	if get_parent().name == "Gray Tiles":
 		sprite.set_texture(gray)
@@ -28,4 +38,26 @@ func _ready():
 	if get_parent().name == "Yellow Tiles":
 		sprite.set_texture(yellow)
 		score = 150
+		
+	position.y = -30
+	var time = rand_range(0,2.5)
+	t.set_wait_time(time)
+	t.start()
+	yield(t, "timeout")
+	$Tween.interpolate_property(self, "position", position, _target, 2.0, Tween.TRANS_BOUCE, Tween.EASE_OUT)
+	$Tween.start()
+	
+func _process(delta):
+	if _dying > 0:
+		_dying += _dying_delta
+		position.y += _dying
+		rotate(_dying_rotate)
+		_dying_rotate+= _dying_rotate_delta
+		$ColorRect.color = $ColorRect.color.linear_interpolate(Color(0,0,0,0),_dying_color_delta)
+	if _dying > _dying_threshhold:
+		queue_free()
+		
+func kill():
+	_dying += _dying_delta
+	$CollisionShape2D.queue_free()
 	
